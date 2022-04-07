63eef5d256791f6a54d028d90b758baaaaa611a5
Admin <Admin@DESKTOP-FJ018V3>
2022-04-08 01:23:01 +0530

    First commit

---
+++ core java1/src/Day6/AssignmentOperator.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int x = 10, y = 20, z = 30; //initialization
+		    x += y; 
+		    y -= x + z; 
+		    z *= x * y;
+		 System.out.println("x = " +x ); 
+		 System.out.println("y = " +y ); 
+		 System.out.println("z = " +z ); 
+
+	}
+
+}
--- core java1/src/Day9/MultipleInheritance.java
+++ core java1/src/Day9/MultipleInheritance.java
@@ -14,7 +14,7 @@ class B
 		System.out.println("Welcome");
 	}
 }
-public class MultipleInheritance extends A1, B
+public class MultipleInheritance extends A1//not allowing Multiple Inheritance
 {
 	public static void main(String[] args) 
 	{
