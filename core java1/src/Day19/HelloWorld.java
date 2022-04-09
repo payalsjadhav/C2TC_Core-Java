ed7d7c6dc74e2f3c511d4ac17cdead7c3bd937ff
Admin <Admin@DESKTOP-FJ018V3>
2022-04-09 23:42:47 +0530

    First commit

---
+++ core java1/src/Day19/HelloWorld.java
@@ -0,0 +1,13 @@
+package Day19;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class HelloWorld extends Applet
+{
+	public void paint (Graphics g)
+	{
+		g.drawString ("Hello World",20,20);
+	}
+
+}
