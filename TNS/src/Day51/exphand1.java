23ac6f27b8f8b71e8aca6459f6257b483b35d02b
schou <schou@SHANKAR>
2022-04-29 15:26:44 +0530

    First Commit

---
+++ TNS/src/Day51/exphand1.java
@@ -0,0 +1,31 @@
+package Day51;
+
+public class exphand1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		try
+		{
+			System.out.println("The First Statements");
+			System.out.println("The Second Statements");
+			System.out.println("The Third Statements");
+			a=100;
+			b=5;
+			int c=a/b;
+			System.out.println("The Fifth Statements");
+			System.out.println("The Sixth Statements");
+			System.out.println("The Seven Statements");
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("The Eighth Statements");
+			System.out.println("The Nine Statements");
+			System.out.println("The Ten Statements");
+			
+		}
+
+	}
+
+}
